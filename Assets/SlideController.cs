bf898442479e6b27537975704dddf0b628f82f9e
grygry12345 <41829095+grygry12345@users.noreply.github.com>
2020-08-11 20:51:24 +0300

    Added quarotation UI

---
+++ Assets/SlideController.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideController : MonoBehaviour
+{
+    private Slider Slide;
+    private InputField TextBox;
+    private Text Info;
+
+    private void Awake()
+    {
+        Slide = gameObject.GetComponentInChildren<Slider>();
+        TextBox = gameObject.GetComponentInChildren<InputField>();
+        Info = gameObject.GetComponentInChildren<Text>();
+    }
+
+    public void OnSliderUpdate() // Update value TextBox when Slider is changed then update the gimbal
+    {
+        TextBox.text = Slide.value.ToString();
+
+        switch (Info.text)
+        {
+            case "Pitch":
+                GimballController.Pitch = Slide.value;
+                GimballController.PitchTransform.eulerAngles = new Vector3(Slide.value, 0, 0);
+                break;
+            case "Yaw":
+                GimballController.Yaw = Slide.value;
+                GimballController.YawTransform.eulerAngles = new Vector3(0,Slide.value,0);
+                break;
+            case "Roll":
+                GimballController.Roll = Slide.value;
+                GimballController.RollTransform.eulerAngles = new Vector3(0, 0, Slide.value);
+                break;
+            default:
+                Debug.LogError("Error in getting angles.");
+                break;
+        }
+    }
+
+    public void OnTextBoxUpdate() // Update value Slider when Textbox is updated then update the gimbal
+    {
+        Slide.value = float.Parse(TextBox.GetComponentInChildren<Text>().text);
+
+        switch (Info.text)
+        {
+            case "Pitch":
+                GimballController.PitchTransform.eulerAngles = new Vector3(Slide.value, 0, 0);
+                break;
+            case "Yaw":
+                GimballController.YawTransform.eulerAngles = new Vector3(0, Slide.value, 0);
+                break;
+            case "Roll":
+                GimballController.RollTransform.eulerAngles = new Vector3(0, 0, Slide.value);
+                break;
+            default:
+                Debug.LogError("Error in getting angles.");
+                break;
+        }
+    }
+}
